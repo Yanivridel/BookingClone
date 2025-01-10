f501c521793f1f15d4cbe1a469698673225bd845
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2025-01-10 15:02:29 +0200

    add property page example

--- Client/src/pages/Property.tsx
+++ Client/src/pages/Property.tsx
@@ -1,23 +1,32 @@
+import { IProperty } from "@/types/propertyTypes";
 import { getPropertyById } from "@/utils/api/propertyApi";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-
+//! Route for testing : http://localhost:5173/property/67810bb5824440db4b93a785
 
 function Property() {
     const { id } = useParams();
-    const [propertyData, setPropertyData] = useState(null);
+    const [propertyData, setPropertyData] = useState<IProperty | null>(null);
 
     useEffect(() => {
-        if(id)
+        if(id) {
             getPropertyById(id)
-            .then(data => setPropertyData(data));
+            .then(data => {
+                setPropertyData(data)
+                console.log(data)
+            });
+        }
     }, [id])
 
+    
     return (
         <div>
-            {propertyData}
-            
+            name : {propertyData?.title} <br/>
+            type : {propertyData?.type} <br/>
+            description : {propertyData?.description} <br/>
+            fine_print : {propertyData?.fine_print} <br/>
+
         </div>
     )
 
