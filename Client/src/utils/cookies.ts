75293383299e8a590e89673c6fae88aac24093e6
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2025-02-10 12:29:02 +0200

    add

--- Client/src/utils/cookies.ts
+++ Client/src/utils/cookies.ts
@@ -1,10 +1,11 @@
 
 export const getCookie = (name: string) => {
-    // const match = document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`));
-    // return match ? match[2] : null;
-    const cookies = document.cookie.split("; ");
-    const tokenCookie = cookies.find((cookie) => cookie.startsWith("token"));
-    return tokenCookie ? tokenCookie.split("=")[1] : null;
+    const match = document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`));
+    console.log("COOKIES", match);
+    return match ? match[2] : null;
+    // const cookies = document.cookie.split("; ");
+    // const tokenCookie = cookies.find((cookie) => cookie.startsWith("token"));
+    // return tokenCookie ? tokenCookie.split("=")[1] : null;
 };
 
 export const removeCookie = (name: string) => {
